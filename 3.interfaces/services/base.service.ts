96ccef7ad89daa1e76d3b3030cd374056d36e5e7
freezeburger <renaud.dubuis@decryptage.net>
2024-05-27 15:46:01 +0200

    Update

--- 3.interfaces/services/base.service.ts
+++ 3.interfaces/services/base.service.ts
@@ -1,22 +1,63 @@
+import { URL } from "../interfaces/semantic.type";
 import { WithID } from "../interfaces/system.types";
 import { CrudService, CrudServiceResponse } from "../pattern/crud.service";
 
-export abstract class BaseService<T extends WithID> implements CrudService<T>{
-    
-    create(target: Omit<T, "id">): Promise<CrudServiceResponse<T>> {
-        throw new Error("Method not implemented.");
+type RestOptions = {
+    method:'GET' | 'POST' | 'PUT' | 'DELETE',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:null|string;
+}
+
+export abstract class BaseService<T extends WithID> implements CrudService<T> {
+
+    protected readonly options:RestOptions = {
+        method:"GET",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:null
+    };
+
+    constructor(public endpoint: `${URL}/${string}s` ) { }
+
+    create(target: Omit<T,"id">): Promise<CrudServiceResponse<T>> {
+
+        const options:RestOptions = {...this.options, method:"POST", body:JSON.stringify(target)}
+
+        return fetch(this.endpoint, options )
+            .then<T>(res => res.json())
+            .then(data => ({ status: "ACCEPTED", data }))
     }
+
     read(target?: T | undefined): Promise<CrudServiceResponse<T | T[]>> {
-        throw new Error("Method not implemented.");
+
+        const url = target ? `${this.endpoint}/${target.id}` : this.endpoint;
+
+        return fetch( url , this.options)
+            .then<T>(res => res.json())
+            .then(data => ({ status: "ACCEPTED", data }))
     }
+
     update(target: T, update: Partial<T>): Promise<CrudServiceResponse<T>> {
-        throw new Error("Method not implemented.");
+        
+        const url = `${this.endpoint}/${target.id}`;
+        const payload = {...target, ...update}
+        const options:RestOptions = {...this.options, method:"PUT", body:JSON.stringify(payload)}
+
+        return fetch(url, options)
+            .then<T>(res => res.json())
+            .then(data => ({ status: "ACCEPTED", data }))
     }
     delete(target: T): Promise<CrudServiceResponse<T>> {
-        throw new Error("Method not implemented.");
+
+        const url = `${this.endpoint}/${target.id}`;
+        const options:RestOptions = {...this.options, method:"DELETE"}
+
+        return fetch(url, options)
+            .then<T>(res => res.json())
+            .then(data => ({ status: "ACCEPTED", data }))
     }
-
-
-    
 
 }
--- 3.interfaces/services/user.service.ts
+++ 3.interfaces/services/user.service.ts
@@ -1,8 +1,16 @@
+import { URL } from "../interfaces/semantic.type";
 import { User } from "../interfaces/user.entity";
-import { CrudService } from "../pattern/crud.service";
+import { BaseService } from "./base.service";
 
 
-export class UserService {
+export class UserService extends BaseService<User> {
+
+    constructor(endpoint: `${URL}/users`  ){
+        super(endpoint);
+        this.read()
+    }
     
 }
 
+
+const u = new UserService('https://127.0.0.1/users')
